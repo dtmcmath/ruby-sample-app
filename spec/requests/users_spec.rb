ea3c73835d138d224711bdd4ca0edc4939d9a59c
David McMath <mcdave@stanford.edu>
2012-01-09 08:18:17 -0600

    Exercise 8.6.2. Clear "password" and "password_confirmation" fields after unsuccessful signup

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,6 +16,23 @@ describe "Users" do
           response.should have_selector('div#error_explanation')
         end.should_not change(User, :count)
       end
+
+      it 'should clear password entries' do
+        lambda do
+          visit signup_path
+          fill_in 'Name',         :with => 'Incorrect'
+          fill_in 'Email',        :with => 'foo@bar.com'
+          fill_in 'Password',     :with => 'foobar'
+          fill_in 'Confirmation', :with => 'barfoo'
+          click_button
+
+          response.should have_selector('input[id="user_password"][type="password"]', :value => '')
+           # do |input|
+            # input.
+          # end
+          response.should have_selector('input[id="user_password_confirmation"][type="password"]', :value => '')
+        end.should_not change(User, :count)
+      end
     end
     
     describe 'success' do
