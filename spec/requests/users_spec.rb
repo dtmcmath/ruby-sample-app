5c92d262c5d643f0fe307b5f0941b5acd2ce72c6
David McMath <mcdave@stanford.edu>
2012-01-10 09:21:28 -0600

    Customize headers in response to signed_in/signed_out state

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -27,9 +27,6 @@ describe "Users" do
           click_button
 
           response.should have_selector('input[id="user_password"][type="password"]', :value => '')
-           # do |input|
-            # input.
-          # end
           response.should have_selector('input[id="user_password_confirmation"][type="password"]', :value => '')
         end.should_not change(User, :count)
       end
@@ -49,6 +46,31 @@ describe "Users" do
           response.should have_selector('div.flash.success', :content => 'Welcome')
           response.should render_template('users/show')
         end.should change(User, :count).by(1)
+      end
+    end
+  end
+  
+  describe 'sign in/out' do
+    describe 'failure' do
+      it 'should not sign a user in' do
+        visit signin_path
+        fill_in :email,    :with => ''
+        fill_in :password, :with => ''
+        click_button
+        response.should have_selector( 'div.flash.error', :content => 'Invalid' )
+      end
+    end
+    
+    describe 'success' do
+      it 'should sign a user in and out' do
+        user = Factory(:user)
+        visit signin_path
+        fill_in :email,    :with => user.email
+        fill_in :password, :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link 'Sign out'
+        controller.should_not be_signed_in
       end
     end
   end
