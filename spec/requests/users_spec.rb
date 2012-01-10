912ebabe2e6d6a4c09391bd5c9ce2ae892870403
David McMath <mcdave@stanford.edu>
2012-01-10 09:31:45 -0600

    Exercise 9.6.1 Refactor signin to "integratin_sign_in" method in spec_helper.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -31,9 +31,9 @@ describe "Users" do
         end.should_not change(User, :count)
       end
     end
-    
+
     describe 'success' do
-      
+
       it 'should make a new user' do
         lambda do
           visit signup_path
@@ -42,14 +42,14 @@ describe "Users" do
           fill_in 'Password',     :with => 'foobar'
           fill_in 'Confirmation', :with => 'foobar'
           click_button
-          
+
           response.should have_selector('div.flash.success', :content => 'Welcome')
           response.should render_template('users/show')
         end.should change(User, :count).by(1)
       end
     end
   end
-  
+
   describe 'sign in/out' do
     describe 'failure' do
       it 'should not sign a user in' do
@@ -60,14 +60,11 @@ describe "Users" do
         response.should have_selector( 'div.flash.error', :content => 'Invalid' )
       end
     end
-    
+
     describe 'success' do
       it 'should sign a user in and out' do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+        integration_sign_in(user)
         controller.should be_signed_in
         click_link 'Sign out'
         controller.should_not be_signed_in
