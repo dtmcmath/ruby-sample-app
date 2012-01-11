31929d2b28d7e7fcb3d4411792ada55ab0783bff
David McMath <mcdave@stanford.edu>
2012-01-10 23:15:39 -0600

    Properly add the uniqueness constraint for emails in the users' table

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,6 @@ ActiveRecord::Schema.define(:version => 20120104040719) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
