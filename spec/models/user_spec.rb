07f9ac983b37f303e6b376c57bf1d07d687a843c
David McMath <mcdave@stanford.edu>
2012-01-11 22:16:35 -0600

    Ordering of posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -140,10 +140,16 @@ describe User do
   describe 'micropost associations' do
     before(:each) do
       @user = User.create(@attr)
+      @mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
+      @mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
     end
     
     it 'should have a microposts attribute' do
       @user.should respond_to(:microposts)
+    end
+    
+    it 'should have the right microposts in the right order' do
+      @user.microposts.should == [@mp2, @mp1]
     end
   end
 end
