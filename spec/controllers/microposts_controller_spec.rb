55702d428583280bc866054552fa93dc2dd2c77b
David McMath <mcdave@stanford.edu>
2012-01-12 09:00:16 -0600

    Deletion of microposts works

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -60,4 +60,35 @@ describe MicropostsController do
       end
     end
   end
+  
+  describe 'DELETE destroy' do
+    before(:each) do
+      @user = Factory(:user)
+      @micropost = Factory(:micropost, :user => @user)      
+    end
+    
+    describe 'for an unauthorized user' do
+      before(:each) do
+        wrong_user = Factory(:user, :email => Factory.next(:email))
+        test_sign_in(wrong_user)
+      end
+      
+      it 'should deny access' do
+        delete :destroy, :id => @micropost
+        response.should redirect_to(root_path)
+      end
+    end
+    
+    describe 'for an authorized user' do
+      before(:each) do
+        test_sign_in(@user)
+      end
+      
+      it 'should destroy the micropost' do
+        lambda do
+          delete :destroy, :id => @micropost
+        end.should change(Micropost, :count).by(-1)
+      end        
+    end
+  end
 end
