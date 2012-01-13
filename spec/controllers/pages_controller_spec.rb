0fa38d4c7e6d8c5f43187e71a184bc7afd9f760a
David McMath <mcdave@stanford.edu>
2012-01-12 22:55:11 -0600

    'Followers' and 'Following' counts show up on home and profile pages, and one can follow or unfollow users.  At least, there are links for following and unfollowing; they don't actually do anything yet.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,14 +4,33 @@ describe PagesController do
   render_views
   
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
+    describe 'when not signed in' do
+      before(:each) do
+        get 'home'
+      end
+      it "should be successful" do
+        response.should be_success
+      end
+      it "should have the right title" do
+        response.should have_selector('title',
+          :content => "#{@base_title} | Home")
+      end
     end
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector('title',
-        :content => 'Ruby on Rails Tutorial Sample App | Home')
+    
+    describe 'when signed in' do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+      
+      it 'should have the right follower/following counts' do
+        get :home
+        response.should have_selector('a', :href => following_user_path(@user),
+                                           :content => '0 following')
+        response.should have_selector('a', :href => followers_user_path(@user),
+                                           :content => '1 follower')
+      end
     end
   end
 
@@ -23,7 +42,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector('title',
-        :content => 'Ruby on Rails Tutorial Sample App | Contact')
+        :content => "#{@base_title} | Contact")
     end
   end
 
@@ -35,7 +54,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector('title',
-        :content => 'Ruby on Rails Tutorial Sample App | About')
+        :content => "#{@base_title} | About")
     end
   end
 
@@ -47,7 +66,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector('title',
-        :content => 'Ruby on Rails Tutorial Sample App | Help')
+        :content => "#{@base_title} | Help")
     end
   end
 
