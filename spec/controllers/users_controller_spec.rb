3d32ca4f2afaf324bf5b047b3c934574af9e9cd4
David McMath <mcdave@stanford.edu>
2012-01-12 23:14:28 -0600

    "Follow" and "Following" actions work

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -351,4 +351,41 @@ describe UsersController do
       end
     end
   end
+  
+  describe 'follow pages' do
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    describe 'when not signed in' do
+      it 'should protect following' do
+        get :following, :id => 1
+        response.should redirect_to(signin_path)
+      end
+      it 'should protect followers' do
+        get :followers, :id => 1
+        response.should redirect_to(signin_path)
+      end
+    end
+    
+    describe 'when signed in' do
+      before(:each) do
+        test_sign_in(@user)
+        @other_user = Factory(:user, :email => Factory.next(:email))
+        @user.follow!(@other_user)
+      end
+      
+      it 'should show user following' do
+        get :following, :id => @user
+        response.should have_selector( 'a', :href => user_path(@other_user),
+                                            :content => @other_user.name )
+      end
+      
+      it 'should show user followers' do
+        get :followers, :id => @other_user
+        response.should have_selector( 'a', :href => user_path(@user),
+                                            :content => @user.name )
+      end
+    end
+  end
 end
