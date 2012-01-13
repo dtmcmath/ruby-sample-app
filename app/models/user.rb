f3402bd8e9c93ccc64df702047f20098994c4bd2
David McMath <mcdave@stanford.edu>
2012-01-12 22:16:12 -0600

    Implement the models for "following" and "followers" attributes of a user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => 'follower_id',
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => 'followed_id',
+                                   :class_name  => 'Relationship',
+                                   :dependent   => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   
   email_regex = /\A[\w\+\-\.]+@[a-z\d\-\.]+\.[a-z]+\Z/i
 
@@ -39,6 +46,18 @@ class User < ActiveRecord::Base
     return user if cookie_salt == user.salt
     # else, a bad salt
     return nil
+  end
+  
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
   
   def feed
