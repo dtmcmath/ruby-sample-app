79ba1507aaef58a3cdef9114de390d23067ec52b
David McMath <mcdave@stanford.edu>
2012-01-13 22:43:40 -0600

    The "follow" and "unfollow" buttons pass the tests

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -3,31 +3,28 @@ require 'spec_helper'
 describe 'Relationships' do
     
   describe 'manage follower/ing' do
-    describe 'for an un-signed-in user' do
-      pending 'should redirect on follow action'
-      pending 'should redirect on unfollow action'
+    before(:each) do
+      @user = Factory(:user)
+      @followed = Factory(:user, :email => Factory.next(:email))
     end
     
     describe 'for a signed-in user' do
       before(:each) do
-        @user = Factory(:user)
         integration_sign_in(@user)
-        @followed = Factory(:user, :email => Factory.next(:email))
       end
       
       it 'should increase following after follow action' do
         visit user_path(@followed)
         click_button 'Follow'
-        response.should redirect_to(user_path(@followed))
+        response.should be_success
         @user.following?(@followed).should be_true
       end
       
       it 'should decrease following after unfollow action' do
         @user.follow!(@followed)
-        @user.following?(@followed).should be_true
-        visit user_path(assigns(@followed))
+        visit user_path(@followed)
         click_button 'relationship_submit'
-        response.should redirect_to(user_path(@followed))
+        response.should be_success
         @user.following?(@followed).should be_false
       end
     end
